0f6689a2331695dcfa791d6b0ca1af36b6d48990
fsdamp <fsdamp@gmail.com>
2022-10-21 13:29:53 +0300

    update filter

--- conf/blog/serializers.py
+++ conf/blog/serializers.py
@@ -4,24 +4,24 @@ from blog.models import Article
 
 
 class ArticlesSerializer(serializers.ModelSerializer):
-    views = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Article
-        fields = ('id', 'title', 'poster', 'views', 'created_at')
-
-    @staticmethod
-    def get_views(obj):
-        return obj.hit_count.hits
-
-
-class ArticleDetailSerializer(serializers.ModelSerializer):
-    views = serializers.SerializerMethodField()
+    views_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
         fields = '__all__'
 
     @staticmethod
-    def get_views(obj):
+    def get_views_count(obj):
+        return obj.hit_count.hits
+
+
+class ArticleDetailSerializer(serializers.ModelSerializer):
+    views_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+    @staticmethod
+    def get_views_count(obj):
         return obj.hit_count.hits
