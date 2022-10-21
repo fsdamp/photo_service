db4f9d47c3fc5ed182e54880085be6c32757bf8e
fsdamp <fsdamp@gmail.com>
2022-10-21 13:31:34 +0300

    update filter

--- conf/blog/urls.py
+++ conf/blog/urls.py
@@ -9,6 +9,6 @@ app_name = 'api_customer_v1'
 urlpatterns = [
     path('articles/', views.ArticlesView.as_view()),
     path('articles/<int:pk>/', views.ArticleDetailView.as_view()),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
